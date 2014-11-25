75c081253315efecc38c5a4f7ddbf1b192c03b00
asakurasol <mamamia1734@gmail.com>
2014-11-25 11:29:00 -0800

    refactored shorten to a directive

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,4 +3,10 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, $http, Links) {
   // Your code here
   angular.extend($scope, Links);
+})
+.directive('shortenDirective',function() {
+  return {
+    restrict: 'AEC',
+    templateUrl: 'app/shorten/shorten.html'
+  }
 });
